c83166b5ffd6cc5bcf81708722d27d5bab5e4bb1
khalil2211 <khalil2211@gmail.com>
2014-06-07 23:33:24 +1000

    The Master opy is working fine Hi guys i have tried to set Hash password to the data base bu fialed,

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -18,49 +18,54 @@ class UsersTableSeeder extends Seeder{
 
     public function run(){
 
+
+
         // create user object
-        $user1 = new User;
+        $user= new User;
         // populate object
-        $user1->u_id = 1;
-        $user1->u_name = 'ivan';
-        $user1->u_password = Hash::make('ivan');
+        $user->u_id = 1;
+        $user->u_name = 'kieran';
+        $user->u_password = Hash::make('kieran');
         // save to table
-        $user1->save();
+        $user->save();
+
 
         // create user object
         $user2= new User;
         // populate object
         $user2->u_id = 2;
-        $user2->u_name = 'khalil';
-        $user2->u_password = Hash::make('khalil');
+        $user2->u_name = 'seungsup';
+        $user2->u_password = Hash::make('seungsup');
         // save to table
         $user2->save();
-
 
         // create user object
         $user3= new User;
         // populate object
         $user3->u_id = 3;
-        $user3->u_name = 'kieran';
-        $user3->u_password = Hash::make('kieran');
+        $user3->u_name = 'sivan';
+        $user3->u_password = Hash::make('ivan');
+
         // save to table
         $user3->save();
 
+
+
         // create user object
-        $user4= new User;
+        $user4 = new User;
         // populate object
         $user4->u_id = 4;
-        $user4->u_name = 'seungsup';
-        $user4->u_password = Hash::make('seungsup');
-
+        $user4->u_name = 'kim';
+        $user4->u_password = Hash::make('kim');
         // save to table
         $user4->save();
 
+
         $user5= new User;
         // populate object
-        $user5->u_id = 5;
-        $user5->u_name = 'seungsup';
-        $user5->u_password = Hash::make('seungsup');
+        $user5->u_id =5;
+        $user5->u_name = 'khalil';
+        $user5->u_password = Hash::make('khalil');
 
         // save to table
         $user5->save();
